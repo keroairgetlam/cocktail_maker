e723c9536831276088b72581f8d67887a196959e
lucie besson <lucie.besson@gmail.com>
2020-06-15 18:27:15 +0200

    added background to list

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,24 @@
 #   cocktail = Ingredient.create!(name: item['strIngredient1'])
 # end
 require 'faker'
-# Cocktail.destroy_all
-# User.destroy_all
+Cocktail.destroy_all
+User.destroy_all
 
-3.times do
+3.times do |indexuser|
   user=User.new
   user.email=Faker::Internet.email
   user.password="password"
-  user.save!
-  10.times do
+  user.save
+  10.times do |indexcocktail|
     cocktail=Cocktail.new
     cocktail.name=Faker::FunnyName.name
     cocktail.user=user
-    cocktail.save!
+    index=indexuser*10+indexcocktail+1
+    if index<10
+      index="0"+index.to_s
+    end
+    cocktail.photo="cocktail#{index}.jpg"
+    cocktail.save
     puts cocktail.name
   end
 end
