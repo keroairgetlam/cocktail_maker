9db3e9c2a4e33980cbe4065e6af020c2451a7d02
lucie besson <lucie.besson@gmail.com>
2020-06-19 12:22:01 +0200

    modified photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,104 +26,139 @@ ingredients["drinks"].each do |ingredient|
 end
 
 
-1.times do
+# 1.times do
+#   user = User.new
+#   user.email = Faker::Internet.email
+#   user.password = "password"
+#   user.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499495/cocktail01_tz1iwt.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail01_tz1iwt.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499497/cocktail02_flpwjr.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail02_flpwjr.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499501/cocktail03_vm7nl7.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail03_vm7nl7.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499506/cocktail04_xntlmd.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail04_xntlmd.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499507/cocktail05_mcn1mo.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail05_mcn1mo.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   puts cocktail.name
+# end
+
+# 1.times do
+#   user = User.new
+#   user.email = Faker::Internet.email
+#   user.password = "password"
+#   user.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499512/cocktail06_iyqgy4.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail06_iyqgy4.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499516/cocktail07_kcqiw2.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail07_kcqiw2.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499517/cocktail08_jrxcje.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail08_jrxcje.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499521/cocktail09_bkvcrk.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail09_bkvcrk.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499528/cocktail10_g3mbo8.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail10_g3mbo8.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   puts cocktail.name
+# end
+
+# 1.times do
+#   user = User.new
+#   user.email = Faker::Internet.email
+#   user.password = "password"
+#   user.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499530/cocktail11_kdrtvx.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail11_kdrtvx.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499535/cocktail12_rbuzgj.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail12_rbuzgj.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499536/cocktail13_ivhvzs.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail13_ivhvzs.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499538/cocktail14_v9fthi.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail14_v9fthi.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499539/cocktail15_na1u3l.jpg')
+#   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
+#   cocktail.photo.attach(io: file, filename: 'cocktail15_na1u3l.jpg', content_type: 'image/jpg')
+#   cocktail.save
+
+#   puts cocktail.name
+# end
+
+3.times do
   user = User.new
   user.email = Faker::Internet.email
   user.password = "password"
   user.save
 
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499495/cocktail01_tz1iwt.jpg')
+  file = URI.open('https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1586&q=80')
   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail01_tz1iwt.jpg', content_type: 'image/jpg')
+  cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/jpg')
   cocktail.save
 
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499497/cocktail02_flpwjr.jpg')
+  file = URI.open('https://images.unsplash.com/photo-1550426735-c33c7ce414ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2443&q=80')
   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail02_flpwjr.jpg', content_type: 'image/jpg')
+  cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/jpg')
   cocktail.save
 
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499501/cocktail03_vm7nl7.jpg')
+  file = URI.open('https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80')
   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail03_vm7nl7.jpg', content_type: 'image/jpg')
+  cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/jpg')
   cocktail.save
 
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499506/cocktail04_xntlmd.jpg')
+  file = URI.open('https://images.unsplash.com/photo-1536599424071-0b215a388ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2304&q=80')
   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail04_xntlmd.jpg', content_type: 'image/jpg')
+  cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/jpg')
   cocktail.save
 
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499507/cocktail05_mcn1mo.jpg')
+  file = URI.open('https://images.unsplash.com/photo-1546171753-97d7676e4602?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail05_mcn1mo.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  puts cocktail.name
-end
-
-1.times do
-  user = User.new
-  user.email = Faker::Internet.email
-  user.password = "password"
-  user.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499512/cocktail06_iyqgy4.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail06_iyqgy4.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499516/cocktail07_kcqiw2.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail07_kcqiw2.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499517/cocktail08_jrxcje.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail08_jrxcje.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499521/cocktail09_bkvcrk.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail09_bkvcrk.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499528/cocktail10_g3mbo8.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail10_g3mbo8.jpg', content_type: 'image/jpg')
+  cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/jpg')
   cocktail.save
 
   puts cocktail.name
 end
 
-1.times do
-  user = User.new
-  user.email = Faker::Internet.email
-  user.password = "password"
-  user.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499530/cocktail11_kdrtvx.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail11_kdrtvx.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499535/cocktail12_rbuzgj.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail12_rbuzgj.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499536/cocktail13_ivhvzs.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail13_ivhvzs.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499538/cocktail14_v9fthi.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail14_v9fthi.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  file = URI.open('https://res.cloudinary.com/airgetlam/image/upload/v1592499539/cocktail15_na1u3l.jpg')
-  cocktail = Cocktail.new(name: Faker::FunnyName.name, user: user)
-  cocktail.photo.attach(io: file, filename: 'cocktail15_na1u3l.jpg', content_type: 'image/jpg')
-  cocktail.save
-
-  puts cocktail.name
-end
